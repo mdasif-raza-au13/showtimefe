e32aa341be9e988440e59a6359862897a7dcbc3e
asif <mdasifr28@gmail.com>
2021-06-16 00:47:09 +0530

    initial deploying

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Login from './Signup/Login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Cards from './Upcomming/index'
+import Body from './components/Body'
+import Booking from './contents/Booking'
+// import Welcome from './Welcome/index'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path="/" component={Welcome}/> */}
+          <Route exact path="/" component={Body} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/upcomming" component={Cards} />
+          <Route exact path="/booking" component={Booking} />
+        </Switch>
+        {/* <Body /> */}
+
+        {/* <Login /> */}
+
+
+
+
+
+
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Show from '../contents/Show'
+
+const Body = () => {
+    return (
+        <>
+          <Show />   
+        </>
+    )
+}
+
+export default Body
